7432669db713ad862be818373333281abfc15a27
wc4i <jaytam@vip.qq.com>
2026-05-20 20:32:58 +0800

    Add category pages and various.html structure

---
+++ js/ost_data.js
@@ -0,0 +1,31 @@
+// 🎵 wc4° 音乐仓库 - 影视原声专属数据库
+// 数据格式： { title: "名称", link: "网盘完整链接", category: "华语区" }
+// 💡 提示：链接必须包含 http:// 或 https:// 开头。
+
+const ostDatabase = [
+    {
+        title: "2020 爱情的开关 电视原声带",
+        category: "华语区",
+        link: "https://url32.ctfile.com/f/4731432-8792479633-c6488d?p=8023"
+    },
+    {
+        title: "2021 八零九零 影视原声带[24-48]",
+        category: "华语区",
+        link: "https://url32.ctfile.com/f/4731432-8795252225-ae1f49?p=8023"
+    },
+    {
+        title: "2021 古董局中局之掠宝清单 影视原声带[HiRes]",
+        category: "华语区",
+        link: "https://url32.ctfile.com/f/4731432-8795246838-41a023?p=8023"
+    },
+    {
+        title: "2022 昔有琉璃瓦 影视剧原声带",
+        category: "华语区",
+        link: "https://url32.ctfile.com/f/4731432-8795252240-fcc99a?p=8023"
+    },
+    {
+        title: "2025 他为什么依然单身 电视剧原声带[24-48]",
+        category: "华语区",
+        link: "https://url32.ctfile.com/f/4731432-8792475280-5466d2?p=8023"
+    }
+];
---
+++ js/various_data.js
@@ -0,0 +1,31 @@
+// 🎵 wc4° 音乐仓库 - 群星闪耀专属数据库
+// 数据格式： { title: "名称", link: "网盘完整链接", category: "华语区" }
+// 💡 提示：链接必须包含 http:// 或 https:// 开头。
+
+const variousDatabase = [
+    {
+        title: "1998 拉阔音乐 Music Is Live 压轴篇98 [香港首版] - 张学友&许志安&苏永康&郑中基&陈晓东",
+        category: "华语区",
+        link: "https://url32.ctfile.com/f/4731432-8787882028-90722d?p=8023"
+    },
+    {
+        title: "2019 众乐纪伍·打包天真",
+        category: "华语区",
+        link: "https://url32.ctfile.com/f/4731432-8789813744-06b9de?p=8023"
+    },
+    {
+        title: "2020 光·谱 鲍勃·迪伦 Retrospectrum Bob Dylan",
+        category: "华语区",
+        link: "https://url32.ctfile.com/f/4731432-8797696593-8d0e51?p=8023"
+    },
+    {
+        title: "2020 《埋班作乐》作品集 2019-2020",
+        category: "华语区",
+        link: "https://url32.ctfile.com/f/4731432-8792140714-73f2cb?p=8023"
+    },
+    {
+        title: "2021 月光山下Am啦_六堆300年多元族群音乐专辑[24-48]",
+        category: "华语区",
+        link: "https://url32.ctfile.com/f/4731432-8790116994-d92091?p=8023"
+    }
+];
