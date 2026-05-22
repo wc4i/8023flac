01e590949f35201f7e5b4229007a9d5722cd4bb2
wc4i <jaytam@vip.qq.com>
2026-05-22 15:35:42 +0800

    fix css path

--- js/ost_data.js
+++ js/ost_data.js
@@ -27,5 +27,110 @@ const ostDatabase = [
         title: "2025 他为什么依然单身 电视剧原声带[24-48]",
         category: "华语区",
         link: "https://url32.ctfile.com/f/4731432-8792475280-5466d2?p=8023"
+    },
+    {
+        title: "2026 《初三的六一儿童节》影视原声带 [24-48]",
+        category: "华语区",
+        link: "https://pan.baidu.com/s/1HFusOAj8c3A3pRXmyT8LNg?pwd=8023"
+    },
+    {
+        title: "2026 低智商犯罪",
+        category: "华语区",
+        link: "https://pan.baidu.com/s/1HYiGVDAnzmhmev9e3KEceg?pwd=8023"
+    },
+    {
+        title: "2026 逐玉 影视原声带 [24-48]",
+        category: "华语区",
+        link: "https://pan.baidu.com/s/1w8LjLmPVLrnMVoFmxdvmUA?pwd=8023"
+    },
+    {
+        title: "2026 江湖夜雨十年灯 影视原声带 [24-48]",
+        category: "华语区",
+        link: "https://pan.baidu.com/s/1tXP3fsgx0eNQvAJvl5TznQ?pwd=8023"
+    },
+    {
+        title: "2026 唐宫奇案之青雾风鸣 影视原声带",
+        category: "华语区",
+        link: "https://pan.baidu.com/s/1FjNFrsa6qzDKwBcbUlvKJA?pwd=8023"
+    },
+    {
+        title: "2026 你好1983 影视原声带 [24-48]",
+        category: "华语区",
+        link: "https://pan.baidu.com/s/15uzDOObEWq0BxCpu8coSVw?pwd=8023"
+    },
+    {
+        title: "2026 方圆八百米 影视剧原声音乐专辑",
+        category: "华语区",
+        link: "https://pan.baidu.com/s/10CmH4Z3iooXRtqgqNyTYDQ?pwd=8023"
+    },
+    {
+        title: "2026 惊蛰无声 影视原声带 [24-48]",
+        category: "华语区",
+        link: "https://pan.baidu.com/s/1M0oMYmu_5dEUzQr775nx0g?pwd=8023"
+    },
+    {
+        title: "2026 岁月有情时 电视剧原声带 [24-48]",
+        category: "华语区",
+        link: "https://pan.baidu.com/s/19d3bu7IVtc-t8MH06KRK5g?pwd=8023"
+    },
+    {
+        title: "2026 玫瑰丛生 影视原声带 [24-48]",
+        category: "华语区",
+        link: "https://pan.baidu.com/s/1tohk4gljXDwq8HluMhQZbg?pwd=8023"
+    },
+    {
+        title: "2026 危险关系 影视原声带 [24-48]",
+        category: "华语区",
+        link: "https://pan.baidu.com/s/1_t6Qd_ozCSbT-rpWwtRHGA?pwd=8023"
+    },
+    {
+        title: "2026 月鳞绮纪 影视原声带 [24-48]",
+        category: "华语区",
+        link: "https://pan.baidu.com/s/118L_N3KJghcRDpE9freCeQ?pwd=8023"
+    },
+    {
+        title: "2026 锁簪 影视原声带 [24-48]",
+        category: "华语区",
+        link: "https://pan.baidu.com/s/1LSXKYxIyVAYJQcHl5R4-8w?pwd=8023"
+    },
+    {
+        title: "2026 生命树 电视剧原声带 [24-48]",
+        category: "华语区",
+        link: "https://pan.baidu.com/s/1r3x2R9XTTsCW9ZBBXC9vpQ?pwd=8023"
+    },
+    {
+        title: "2026 你是迟来的欢喜 影视原声带 [24-48]",
+        category: "华语区",
+        link: "https://pan.baidu.com/s/1Z6R1BWzZBvi19KRWz6onOA?pwd=8023"
+    },
+    {
+        title: "2026 佳偶天成 影视剧原声音乐专辑",
+        category: "华语区",
+        link: "https://pan.baidu.com/s/1YGsk9rmqfYa_m7jmoaJTmg?pwd=8023"
+    },
+    {
+        title: "2026 爱情没有神话 电视剧原声带",
+        category: "华语区",
+        link: "https://pan.baidu.com/s/1RCwieQoaQsOQPq4TpoXFug?pwd=8023"
+    },
+    {
+        title: "2026 纯真年代的爱情 影视原声带 [24-48]",
+        category: "华语区",
+        link: "https://pan.baidu.com/s/1UTE701T4S1_esEpHu_zncw?pwd=8023"
+    },
+    {
+        title: "2026 冰湖重生 影视剧原声带 [24-48]",
+        category: "华语区",
+        link: "https://pan.baidu.com/s/1AZ1oQFLcYSqSgRnFAnY3hg?pwd=8023"
+    },
+    {
+        title: "2026 家事法庭 电视剧原声带 [24-48]",
+        category: "华语区",
+        link: "https://pan.baidu.com/s/1r2w4U97qJO47CRnMO_b6xQ?pwd=8023"
+    },
+    {
+        title: "2026 人间惊鸿客 影视原声带 [24-48]",
+        category: "华语区",
+        link: "https://pan.baidu.com/s/1zUenuFD9tu1tvkvE5ABh8w?pwd=8023"
     }
 ];
