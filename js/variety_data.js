45498a355a82cdcbff9b68b80408096b3289004d
wc4i <jaytam@vip.qq.com>
2026-07-01 13:22:03 +0800

    fix css path

--- js/variety_data.js
+++ js/variety_data.js
@@ -1,2 +1,6 @@
 // 综艺集锦资源数据库
-const varietyDatabase = [];
+const varietyDatabase = [
+    { title: "乐队的夏天 第1季 2019", link: "https://pan.baidu.com/s/1QPkcPQauh5uoykqXN3zAPw?pwd=8023" },
+    { title: "乐队的夏天 第2季 2020", link: "https://pan.baidu.com/s/1hgQ8KAS9I0PJWwNTk794Zg?pwd=8023" },
+    { title: "乐队的夏天 第3季 2023", link: "https://pan.baidu.com/s/1wxmYRmcF8hUemUeq6mwQQA?pwd=8023" },
+];
